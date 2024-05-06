0589445cffdad33d22532bb342741a8b3f739ba3
srilatha0628 <141114144+srilatha0628@users.noreply.github.com>
2024-05-06 12:07:36 +0530

    Add files via upload

---
+++ apigatewayapp/src/main/java/com/capstone/tenantdetails/ApigatewayappApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.tenantdetails;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayappApplication.class, args);
+	}
+
+}
---
+++ apigatewayapp/src/test/java/com/capstone/tenantdetails/ApigatewayappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.tenantdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
